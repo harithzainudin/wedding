928c1491654e7695dc895c3397cfae02ab362427
Harith Zainudin <harithzainudin7@gmail.com>
2026-01-12 01:28:05 +0800

    Add Contact Hub with WhatsApp buttons

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import HeroSection from "@/components/sections/HeroSection.vue";
 import DetailsSection from "@/components/sections/DetailsSection.vue";
 import ScheduleSection from "@/components/sections/ScheduleSection.vue";
+import ContactSection from "@/components/sections/ContactSection.vue";
 import RsvpSection from "@/components/sections/RsvpSection.vue";
 import StickyNavigation from "@/components/ui/StickyNavigation.vue";
 </script>
@@ -11,6 +12,7 @@ import StickyNavigation from "@/components/ui/StickyNavigation.vue";
     <HeroSection />
     <DetailsSection />
     <ScheduleSection />
+    <ContactSection />
     <RsvpSection />
     <StickyNavigation />
   </main>
