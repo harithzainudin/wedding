cb6da36163f6ff831c5e34286e24fb2579b952c3
Harith Zainudin <harithzainudin7@gmail.com>
2026-01-12 23:42:14 +0800

    Upgrade to nodejs v22

--- backend/infra/api.ts
+++ backend/infra/api.ts
@@ -1,5 +1,9 @@
 import { table } from "./database";
 
+const functionConfig = {
+  runtime: "nodejs22.x" as const,
+};
+
 export const api = new sst.aws.ApiGatewayV2("WeddingApi", {
   cors: {
     allowOrigins: [
@@ -16,12 +20,14 @@ export const api = new sst.aws.ApiGatewayV2("WeddingApi", {
 api.route("POST /rsvp", {
   handler: "src/functions/rsvp/submit.handler",
   link: [table],
+  ...functionConfig,
 });
 
 // GET /rsvp - List all RSVPs (admin)
 api.route("GET /rsvp", {
   handler: "src/functions/rsvp/list.handler",
   link: [table],
+  ...functionConfig,
 });
 
 // Function to add admin routes with secrets
@@ -35,23 +41,27 @@ export function addAdminRoutes(
   api.route("POST /admin/login", {
     handler: "src/functions/admin/login.handler",
     link: [adminPassword, table],
+    ...functionConfig,
   });
 
   // POST /admin/users - Create admin user (with email notification)
   api.route("POST /admin/users", {
     handler: "src/functions/admin/create.handler",
     link: [table, brevoApiKey, senderEmail, adminLoginUrl],
+    ...functionConfig,
   });
 
   // GET /admin/users - List admin users
   api.route("GET /admin/users", {
     handler: "src/functions/admin/list.handler",
     link: [table],
+    ...functionConfig,
   });
 
   // DELETE /admin/users/{username} - Delete admin user
   api.route("DELETE /admin/users/{username}", {
     handler: "src/functions/admin/delete.handler",
     link: [table],
+    ...functionConfig,
   });
 }
