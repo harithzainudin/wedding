2d3da645bceb46c43cb1db3a9814cc5be7584262
Harith Zainudin <harithzainudin7@gmail.com>
2026-01-13 21:52:26 +0800

    FIX: scrolling behaviour

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,8 @@
+// Disable browser's automatic scroll restoration on page refresh
+if ("scrollRestoration" in history) {
+  history.scrollRestoration = "manual";
+}
+
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
